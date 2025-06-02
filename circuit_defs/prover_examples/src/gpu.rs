5d7a38299f03819ef8830c5629e67f70ff6c316c
mm <mm@matterlabs.dev>
2025-06-02 11:10:54 +0200

    increase host memory to 8GB

--- circuit_defs/prover_examples/src/gpu.rs
+++ circuit_defs/prover_examples/src/gpu.rs
@@ -49,8 +49,8 @@ use crate::{NUM_QUERIES, POW_BITS};
 
 pub fn initialize_host_allocator_if_needed() {
     if !MemPoolProverContext::is_host_allocator_initialized() {
-        // allocate 4 x 1 GB ((1 << 8) << 22) of pinned host memory with 4 MB (1 << 22) chunking
-        MemPoolProverContext::initialize_host_allocator(4, 1 << 8, 22).unwrap();
+        // allocate 8 x 1 GB ((1 << 8) << 22) of pinned host memory with 4 MB (1 << 22) chunking
+        MemPoolProverContext::initialize_host_allocator(8, 1 << 8, 22).unwrap();
     }
 }
 
