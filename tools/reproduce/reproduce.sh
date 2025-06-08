2d25274cb994974aa005a38dac1e22e46be5a748
mm <mm@matterlabs.dev>
2025-06-08 09:09:47 +0200

    script to reproduce and a docker file

---
+++ tools/reproduce/reproduce.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Make sure to run from the main zksync-airbender directory.
+
+# create a fresh docker
+docker build -t airbender-verifiers  -f tools/reproduce/Dockerfile .
+
+docker create --name verifiers airbender-verifiers
+
+FILES=(
+    base_layer.bin
+    recursion_layer.bin
+    final_recursion_layer.bin
+    base_layer_with_output.bin
+    recursion_layer_with_output.bin
+    final_recursion_layer_with_output.bin
+    universal.bin
+    universal_no_delegation.bin
+    verifier_test.bin
+    universal.reduced.vk.json
+    universal_no_delegation.final.vk.json
+    recursion_layer.reduced.vk.json
+    recursion_layer_no_delegation.final.vk.json
+    final_recursion_layer.final.vk.json
+)
+
+for FILE in "${FILES[@]}"; do
+    docker cp verifiers:/zksync-airbender/tools/verifier/$FILE tools/verifier/
+done
+
+
+docker rm verifiers
