a9da9063e8829b4cd7d8430ed1cc15f2a12c9af9
mm <mm@matterlabs.dev>
2025-08-23 15:24:48 +0200

    Added missing entries to reproduce script.

--- tools/reproduce/reproduce.sh
+++ tools/reproduce/reproduce.sh
@@ -14,17 +14,24 @@ docker create --name verifiers airbender-verifiers
 FILES=(
     base_layer.bin
     recursion_layer.bin
+    recursion_log_23_layer.bin
+    recursion_layer_no_delegation.bin
     final_recursion_layer.bin
     base_layer_with_output.bin
     recursion_layer_with_output.bin
+    recursion_log_23_layer_with_output.bin
+    recursion_layer_no_delegation_with_output.bin
     final_recursion_layer_with_output.bin
     universal.bin
     universal_no_delegation.bin
+    base_layer.reduced.vk.json
     universal.reduced.vk.json
     universal_no_delegation.final.vk.json
     recursion_layer.reduced.vk.json
     recursion_layer_no_delegation.final.vk.json
     final_recursion_layer.final.vk.json
+    universal.reduced_log23.vk.json
+    recursion_log_23_layer.reduced.vk.json
 )
 
 for FILE in "${FILES[@]}"; do
