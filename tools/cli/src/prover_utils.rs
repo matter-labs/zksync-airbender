4e451fbe03f9065bd11d0829f6f8e6d422c73a6b
mm <mm@matterlabs.dev>
2025-10-27 23:03:42 +0100

    pub use

--- tools/cli/src/prover_utils.rs
+++ tools/cli/src/prover_utils.rs
@@ -1,9 +1,8 @@
 use clap::ValueEnum;
 use execution_utils::verifier_binaries::UNIVERSAL_CIRCUIT_VERIFIER;
-use execution_utils::Machine;
-use execution_utils::{
+pub use execution_utils::{
     generate_oracle_data_for_universal_verifier, generate_oracle_data_from_metadata_and_proof_list,
-    get_padded_binary, ProgramProof, ProofList, ProofMetadata, RecursionStrategy,
+    get_padded_binary, Machine, ProgramProof, ProofList, ProofMetadata, RecursionStrategy,
 };
 use verifier_common::parse_field_els_as_u32_from_u16_limbs_checked;
 
