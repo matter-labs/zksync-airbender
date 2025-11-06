90795cdb836677bdffacbe53e103fb02caa53a38
mcarilli <mcarilli@gmail.com>
2025-11-06 23:59:52 +0000

    undo more debugging diffs

--- prover/src/tests/unrolled/word_specialized.rs
+++ prover/src/tests/unrolled/word_specialized.rs
@@ -1704,20 +1704,20 @@ pub fn run_basic_unrolled_test_with_word_specialization_impl(
         permutation_argument_accumulator.mul_assign(&proof.permutation_grand_product_accumulator);
     }
 
-    // // Machine state permutation ended
-    // {
-    //     for (pc, ts) in write_set.iter().copied() {
-    //         if read_set.contains(&(pc, ts)) == false {
-    //             panic!("read set doesn't contain a pair {:?}", (pc, ts));
-    //         }
-    //     }
+    // Machine state permutation ended
+    {
+        for (pc, ts) in write_set.iter().copied() {
+            if read_set.contains(&(pc, ts)) == false {
+                panic!("read set doesn't contain a pair {:?}", (pc, ts));
+            }
+        }
 
-    //     for (pc, ts) in read_set.iter().copied() {
-    //         if write_set.contains(&(pc, ts)) == false {
-    //             panic!("write set doesn't contain a pair {:?}", (pc, ts));
-    //         }
-    //     }
-    // }
+        for (pc, ts) in read_set.iter().copied() {
+            if write_set.contains(&(pc, ts)) == false {
+                panic!("write set doesn't contain a pair {:?}", (pc, ts));
+            }
+        }
+    }
 
     if true {
         println!("Will try to prove memory inits and teardowns circuit");
@@ -2001,43 +2001,43 @@ pub fn run_basic_unrolled_test_with_word_specialization_impl(
     dbg!(permutation_argument_accumulator);
     dbg!(delegation_argument_accumulator);
 
-    // // inits and teardowns
-    // {
-    //     let expected_init_set: Vec<_> = memory_read_set.difference(&memory_write_set).collect();
-    //     let expected_teardown_set: Vec<_> = memory_write_set.difference(&memory_read_set).collect();
-    //     assert_eq!(expected_init_set.len(), expected_teardown_set.len());
+    // inits and teardowns
+    {
+        let expected_init_set: Vec<_> = memory_read_set.difference(&memory_write_set).collect();
+        let expected_teardown_set: Vec<_> = memory_write_set.difference(&memory_read_set).collect();
+        assert_eq!(expected_init_set.len(), expected_teardown_set.len());
 
-    //     for (is_register, addr, ts, init_value) in expected_init_set.iter() {
-    //         assert!(*is_register == false);
-    //         assert_eq!(
-    //             *ts, 0,
-    //             "init timestamp is invalid for memory address {}",
-    //             addr
-    //         );
-    //         assert_eq!(
-    //             *init_value, 0,
-    //             "init value is invalid for memory address {}",
-    //             addr
-    //         );
-    //     }
-    //     for (is_register, addr, ts, _) in expected_teardown_set.iter() {
-    //         assert!(*is_register == false);
-    //         assert!(
-    //             *ts > INITIAL_TIMESTAMP,
-    //             "teardown timestamp is invalid for memory address {}",
-    //             addr
-    //         );
-    //     }
+        for (is_register, addr, ts, init_value) in expected_init_set.iter() {
+            assert!(*is_register == false);
+            assert_eq!(
+                *ts, 0,
+                "init timestamp is invalid for memory address {}",
+                addr
+            );
+            assert_eq!(
+                *init_value, 0,
+                "init value is invalid for memory address {}",
+                addr
+            );
+        }
+        for (is_register, addr, ts, _) in expected_teardown_set.iter() {
+            assert!(*is_register == false);
+            assert!(
+                *ts > INITIAL_TIMESTAMP,
+                "teardown timestamp is invalid for memory address {}",
+                addr
+            );
+        }
 
-    //     for ((_, addr0, _, _), (_, addr1, _, _)) in
-    //         expected_init_set.iter().zip(expected_teardown_set.iter())
-    //     {
-    //         assert_eq!(*addr0, *addr1);
-    //     }
+        for ((_, addr0, _, _), (_, addr1, _, _)) in
+            expected_init_set.iter().zip(expected_teardown_set.iter())
+        {
+            assert_eq!(*addr0, *addr1);
+        }
 
-    //     assert_eq!(total_unique_teardowns, expected_teardown_set.len());
-    // }
+        assert_eq!(total_unique_teardowns, expected_teardown_set.len());
+    }
 
-    // assert_eq!(permutation_argument_accumulator, Mersenne31Quartic::ONE);
-    // assert_eq!(delegation_argument_accumulator, Mersenne31Quartic::ZERO);
+    assert_eq!(permutation_argument_accumulator, Mersenne31Quartic::ONE);
+    assert_eq!(delegation_argument_accumulator, Mersenne31Quartic::ZERO);
 }
