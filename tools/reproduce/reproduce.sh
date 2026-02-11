b8a50b91e285da38c1cdf6fb36d46b4c0f6fd756
Igor Aleksanov <popzxc@yandex.ru>
2026-02-11 12:37:12 +0400

    Add 100bit security verifiers

--- tools/reproduce/reproduce.sh
+++ tools/reproduce/reproduce.sh
@@ -14,26 +14,8 @@ docker create --name verifiers airbender-verifiers
 FILES=(
     recursion_in_unified_layer.bin
     recursion_in_unrolled_layer.bin
-    # base_layer.bin
-    # recursion_layer.bin
-    # recursion_log_23_layer.bin
-    # recursion_layer_no_delegation.bin
-    # final_recursion_layer.bin
-    # base_layer_with_output.bin
-    # recursion_layer_with_output.bin
-    # recursion_log_23_layer_with_output.bin
-    # recursion_layer_no_delegation_with_output.bin
-    # final_recursion_layer_with_output.bin
-    # universal.bin
-    # universal_no_delegation.bin
-    # base_layer.reduced.vk.json
-    # universal.reduced.vk.json
-    # universal_no_delegation.final.vk.json
-    # recursion_layer.reduced.vk.json
-    # recursion_layer_no_delegation.final.vk.json
-    # final_recursion_layer.final.vk.json
-    # universal.reduced_log23.vk.json
-    # recursion_log_23_layer.reduced.vk.json
+    recursion_in_unified_layer_security_100_bits.bin
+    recursion_in_unrolled_layer_security_100_bits.bin
 )
 
 for FILE in "${FILES[@]}"; do
