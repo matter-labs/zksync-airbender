0909a441f0a846f8a69865d20a4ceb4442d16508
Alexander <alex.m.vlasov@gmail.com>
2026-04-07 15:04:35 +0300

    implement special rorations via MOP.i

--- blake2s_u32/src/asm_utils.rs
+++ blake2s_u32/src/asm_utils.rs
@@ -1,13 +1,16 @@
 #[inline(always)]
-// #[cfg(target_arch = "riscv32")]
-#[cfg(all(target_arch = "riscv32", target_feature = "zbb"))]
+#[cfg(all(
+    target_arch = "riscv32",
+    target_feature = "zbb",
+    not(feature = "mop_extension")
+))]
 pub fn rotate_right<const AMT: u32>(value: u32) -> u32 {
     let mut output;
     unsafe {
         core::arch::asm!(
             "rori {rd}, {rs1}, {amt}",
             rs1 = in(reg) value,
-            rd = out(reg) output,
+            rd = lateout(reg) output,
             amt = const AMT,
             options(nomem, nostack, preserves_flags)
         );
@@ -17,8 +20,32 @@ pub fn rotate_right<const AMT: u32>(value: u32) -> u32 {
 }
 
 #[inline(always)]
-// #[cfg(not(target_arch = "riscv32"))]
-#[cfg(not(all(target_arch = "riscv32", target_feature = "zbb")))]
+#[cfg(not(all(
+    target_arch = "riscv32",
+    target_feature = "zimop",
+    feature = "mop_extension"
+)))]
 pub fn rotate_right<const AMT: u32>(value: u32) -> u32 {
     value.rotate_right(AMT)
+}
+
+#[inline(always)]
+#[cfg(all(
+    target_arch = "riscv32",
+    target_feature = "zimop",
+    feature = "mop_extension"
+))]
+pub fn rotate_right<const AMT: u32>(value: u32) -> u32 {
+    let mut output;
+    unsafe {
+        core::arch::asm!(
+            "mop.r.{amt} {rd}, {rs1}",
+            rs1 = in(reg) value,
+            rd = lateout(reg) output,
+            amt = const AMT,
+            options(nomem, nostack, preserves_flags)
+        );
+    }
+
+    output
 }
