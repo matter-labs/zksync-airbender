267a4446a013547643610d7ecc41f362c7e715a3
Igor Aleksanov <popzxc@yandex.ru>
2026-05-05 11:56:47 +0400

    feat: Implement keccak_f1600 (#287)
    Right now the impl is split between this repo and
    airbender-platform/zksync-os, so it's better to expose a combined
    `keccak_f1600` entrypoint that will not expose internal invariants.

--- prover/src/definitions/leaf_inclusion_verifier/blake2s_for_everything.rs
+++ prover/src/definitions/leaf_inclusion_verifier/blake2s_for_everything.rs
@@ -15,7 +15,6 @@ impl LeafInclusionVerifier for Blake2sForEverythingVerifier {
     }
 
     #[unroll::unroll_for_loops]
-    #[expect(unreachable_code, unused_assignments, unused_variables)]
     unsafe fn verify_leaf_inclusion<
         I: NonDeterminismSource,
         const CAP_SIZE: usize,
