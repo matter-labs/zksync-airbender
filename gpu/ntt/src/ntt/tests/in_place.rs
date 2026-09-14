02801e723926fe3e3b7f7190f7c869c6d92ddec0
Robert Remen <robik75@gmail.com>
2026-09-14 12:57:35 +0000

    refactor(ntt): remove unnecessary no-CUDA test guards

--- gpu/ntt/src/ntt/tests/in_place.rs
+++ gpu/ntt/src/ntt/tests/in_place.rs
@@ -11,7 +11,6 @@ use super::super::{
 };
 use super::make_context;
 
-#[cfg(not(no_cuda))]
 fn inverse_roundtrip(log_n: usize, log_f: usize, force_two_pass: bool) {
     let context = make_context();
     let stream = context.get_exec_stream();
@@ -159,7 +158,6 @@ fn inverse_roundtrip(log_n: usize, log_f: usize, force_two_pass: bool) {
 macro_rules! inverse_case {
     ($name:ident, $n:literal, $f:literal, $two:literal) => {
         #[test]
-        #[cfg(not(no_cuda))]
         fn $name() {
             inverse_roundtrip($n, $f, $two);
         }
--- gpu/ntt/src/ntt/tests/retained_monomials.rs
+++ gpu/ntt/src/ntt/tests/retained_monomials.rs
@@ -179,7 +179,6 @@ fn run(log_n: usize, log_f: usize, force_two_pass: bool) {
 macro_rules! case {
     ($name:ident, $n:literal, $f:literal, $two:literal) => {
         #[test]
-        #[cfg(not(no_cuda))]
         fn $name() {
             run($n, $f, $two);
         }
